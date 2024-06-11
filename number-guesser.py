b464fb594a746147d1cab68b7d8fe8e045da6982
Andrew <andrew@andrewperlas.com>
2024-06-11 13:35:14 -1000

    switch try top_range back to top_range.isdigit()

--- number-guesser.py
+++ number-guesser.py
@@ -2,16 +2,30 @@ import random
 
 top_range = input("Type a number: ")
 
-try:
+if top_range.isdigit():
     top_range = int(top_range)
     
     if top_range <= 0:
         print("Please type a number larger than 0")
         quit()
-except ValueError:
+else:
     print("Please type a number")
     quit()
 
-print(f"The number you typed was {top_range}")
-
 random_number = random.randint(0, top_range)
+guess = 0
+
+while True:
+    guess += 1
+    user_guess = input("Guess the number: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print("Please guess a number")
+        continue
+
+    if user_guess == random_number:
+        print(f"You got it in {guess} guesses!")
+        break
+    else:
+        print("Please guess again")
